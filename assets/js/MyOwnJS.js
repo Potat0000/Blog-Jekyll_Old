28c7ca85ab7fb934c02398ac68c5666cd678ab84
gyj1109 <gyj1109@126.com>
2015-11-20 16:35:23 +0800

    update

--- assets/js/MyOwnJS.js
+++ assets/js/MyOwnJS.js
@@ -2,10 +2,10 @@ function EnterPress(e) {
 	var e = e || window.event;
 	if (e.keyCode == 13) {
 		var child = document.getElementById("password");
-		if ($("#password").text() == "1234") {
+		if ($("#password").val() == "1234") {
 			$("#password").remove();
 		} else {
-			$("#password").text() = "";
+			$("#password").val() = "";
 		}
 	}
 }
